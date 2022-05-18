1147b5fb648a6dd9da6086e833dfef8b118e169f
Veeresh B V <Veereshbv04@gmail.com>
2022-05-18 10:01:46 +0530

    card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,5 @@
 // import { doepicshit } from "../../../public/assets/book-images"
-
+import "./Card.css"
 export default function Card({ product }) {
     console.log(product)
     const { productImage, title, author, price: { original, discounted, discount } } = product
@@ -13,15 +13,18 @@ export default function Card({ product }) {
 
             <div className="card-content">
                 <h3 className="card-title">{title}</h3>
-                <h5>{author}</h5>
+
                 <div>
-                    <span className="price highlight"><strong>{original}  </strong></span>
-                    <span className="price highlight">{discounted}</span>
+                    <text style={{ marginRight: "4rem" }}>{author}</text>
+                    <span className="price highlight"><strong>{discounted}  </strong></span>
+                    <span className="price highlight">{original}</span>
                 </div>
+                <button className="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
+                <br />
                 <button className="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
                 <span className="fa fa-heart-o heart w3-xxlarge"></span>
             </div>
-        </div>
+        </div >
 
     )
 }
