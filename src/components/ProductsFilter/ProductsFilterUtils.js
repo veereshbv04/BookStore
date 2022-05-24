70cc9c5fa65a97e6ba8197ce5ab4bf95c325ca64
Veeresh B V <Veereshbv04@gmail.com>
2022-05-24 16:29:05 +0530

    feat: filter by price, rating

--- src/components/ProductsFilter/ProductsFilterUtils.js
+++ src/components/ProductsFilter/ProductsFilterUtils.js
@@ -1,5 +1,15 @@
-const getFilteredProducts = ()=>{
-    console.log("from product filter")
+const sortBy = ({sortBy}, products)=>{
+    const sortedProducts = [...products]
+    switch(sortBy){
+        case "HIGH-TO-LOW":
+            return sortedProducts.sort((product1, product2)=>product2.price.discounted - product1.price.discounted)
+        case "LOW-TO-HiGH":
+            return sortedProducts.sort((product1,product2) => product1.price.discounted - product2.price.discounted)
+        default:
+            return sortedProducts
+    }
 }
 
-export {getFilteredProducts}
+const ratedProducts =({rating}, products)=>{
+    return products.filter(product => product.rating >=rating)
+}
