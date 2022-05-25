b85b304fe068b4e12593cb0baf3f2bc1562b11c7
Veeresh B V <Veereshbv04@gmail.com>
2022-05-25 23:21:15 +0530

    feat: Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,29 @@
 export default function Header() {
     return (
         <header>
-            <h1>This is  Header</h1>
+            <nav className="navbar">
+                <a href="/pages/home.html"><div className="navbar-title">BookStore</div></a>
+
+                {/* <div >
+                    <ul className="navbar-catagory">
+                        <li><a href="/pages/home.html">Fiction</a></li>
+                        <li><a href="/pages/home.html">Children Books</a></li>
+                        <li><a href="/pages/home.html">Technical Books</a></li>
+                        <li><a href="/pages/home.html">All Books</a></li>
+                    </ul>
+                </div> */}
+
+                <div >
+                    <input className="navbar-search" placeholder="search here" />
+                </div>
+                <div className="icons">
+                    <a href="/pages/wishlist.html"><span className="fa fa-heart"></span></a>
+                    <div className="badge-icon">2</div>
+                    <a href="/pages/cart.html"><span className="fa fa-shopping-cart"></span></a>
+                </div>
+                <button className="btn btn-primary"><a href="/index.html">Login</a></button>
+
+            </nav>
         </header>
     )
 }
