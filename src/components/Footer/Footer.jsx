d7db1c2441edc4eb977a1ff907d05bd06fff6b41
Veeresh B V <Veereshbv04@gmail.com>
2022-05-27 00:21:08 +0530

    feat: footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+export default function Footer(){
+    return (
+        <footer class="footer">
+            <section>
+                <span class="footer-title">Get In touch
+                </span>
+                <ul>
+                    <li class="footer-links">abcdefghijklmn</li>
+                    <li class="footer-links">abcdefghijklmn</li>
+                    <li class="footer-links">abcdefghijklmn</li>
+                </ul>
+            </section>
+            <section>
+                <span class="footer-title">Quick Links
+                </span>
+                <ul>
+                    <li class="footer-links">abcdefghijklmn</li>
+                    <li class="footer-links">abcdefghijklmn</li>
+                    <li class="footer-links">abcdefghijklmn</li>
+                </ul>
+            </section>
+            <section>
+                <span class="footer-title">Information
+                </span>
+                <ul>
+                    <li class="footer-links">abcdefghijklmn</li>
+                    <li class="footer-links">abcdefghijklmn</li>
+                    <li class="footer-links">abcdefghijklmn</li>
+                </ul>
+            </section>
+            <section>
+                <span class="footer-title">Follow Us
+                </span>
+                <ul>
+                    <li class="footer-links">abcdefghijklmn</li>
+                    <li class="footer-links">abcdefghijklmn</li>
+                    <li class="footer-links">abcdefghijklmn</li>
+                    <span> 2022 Copyright Reserved</span>
+                </ul>
+            </section>
+        </footer>
+    )
+}
