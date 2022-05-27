821c68398888b7449aa3cceb23f63efe23f1c7f3
Veeresh B V <Veereshbv04@gmail.com>
2022-05-27 21:36:10 +0530

    feat: inputhandler

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -9,7 +9,6 @@ export default function Login() {
                 <input className="input" placeholder="Enter Email Id" type="email" />
                 <input className="input" placeholder="Enter Password" type="password" />
 
-                {/* <Link to="/login"><small class="error-message green"> Don't have an acoount ?</small></Link> */}
                 
                 <Link to="/signup"><small className="error-message green">Don't have an acoount ?</small></Link>
                 <Link to="/"><button class="btn btn-primary">Sign Up</button></Link>
--- src/pages/Authentication/SignUp.jsx
+++ src/pages/Authentication/SignUp.jsx
@@ -1,18 +1,44 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 export default function SignUp() {
+
+    const [newUserData, setNewUserData] = useState({
+        firstName:"",
+        lastName:"",
+        emailId:"",
+        password:"",
+        confirmPassword:""
+    })
+
+    function userInputHandler(event){
+        const {name, value} = event.target;
+        
+        setNewUserData(prev => ({...prev, [name]:value}))
+    }
+
+    function sendNewUserData(data){
+        
+    }
+
+    function signUpHandler(e){
+        e.preventDefault()
+        sendNewUserData(newUserData)
+    }
+
     return (
-        <div class="center-div">
-            <form class="form">
+        <div className="center-div">
+            <form className="form">
                
-                <h2 class="subheading">Create Account</h2>
-                <input class="input" placeholder="Enter First Name" type="text" />
-                <input class="input" placeholder="Enter Second Name" type="text" />
-                <input class="input" placeholder="Enter Email Id" type="email" />
-                <input class="input" placeholder="Create Password" type="password" />
-                <button class="btn btn-primary"><a href="/pages/home.html">Sign Up</a></button>
+                <h2 className="subheading">Create Account</h2>
+                <input className="input" placeholder="Enter First Name" type="text" name="firstName" required onChange={userInputHandler} />
+                <input className="input" placeholder="Enter Last Name" type="text" name="lastName" required onChange={userInputHandler}/>
+                <input className="input" placeholder="Enter Email Id" type="email" name="emailId" required onChange={userInputHandler}/>
+                <input className="input" placeholder="Create Password" type="password" name="password" required onChange={userInputHandler}/>
+                <input className="input" placeholder="ConfirmPassword" type="password" name="confirmPassword" required onChange={userInputHandler}/>
+                <button className="btn btn-primary" onClick={signUpHandler}>Sign Up</button>
                 
-                <Link to="/login"><small class="error-message green">Already have an account?</small></Link>
+                <Link to="/login"><small className="error-message green">Already have an account?</small></Link>
             </form>
         </div>
     )
