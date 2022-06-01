e4ad2c548824e89a0e1ae1dd9173dc1384628466
Veeresh B V <Veereshbv04@gmail.com>
2022-06-01 12:31:08 +0530

    feat: added cart and wishlist body

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,3 +1,68 @@
 export default function Cart() {
-    return (<div>This is cart page from pages folder</div>)
+    return (
+        <section className="container center-div">
+            <div className="card card-horizontal shadow">
+                <img className="img-horizontal" src="/assets/doepicshit.jpg" alt="sample image" />
+                    <div className="card-content">
+
+                        <h2 className="card-title">Do Epic Shit(paperback) by Ankur warikko</h2>
+                        <small>Sapna Publication</small>
+                        <h3 className="price">₹299 <small className="offer-saving strike">₹500</small></h3>
+                        <span className="offer-saving">40% Offer</span>
+
+                        <div className="quantity">
+                            <span className="plus">+</span>
+                            <input type="number" />
+                                <span className="minus">-</span>
+                        </div>
+                    </div>
+            </div>
+
+            <div className="card card-horizontal shadow">
+                <img className="img-horizontal" src="/assets/doepicshit.jpg" alt="sample image" />
+                    <div className="card-content">
+
+                        <h2 className="card-title">Do Epic Shit(paperback) by Ankur warikko</h2>
+                        <small>Sapna Publication</small>
+                        <h3 className="price">₹299 <small className="offer-saving strike">₹500</small></h3>
+                        <span className="offer-saving">40% Offer</span>
+
+                        <div className="quantity">
+                            <span className="plus">+</span>
+                            <input type="number" />
+                                <span className="minus">-</span>
+                        </div>
+                    </div>
+            </div>
+            
+            
+            <div className="card card-vertical shadow">
+
+                <div className="card-content">
+                    <h1 className="card-title" style={{color:"green"}}>Price Details</h1>
+                    <div className="cart-price">
+                        <div className="price-field">
+                            <span>Price</span>
+                            <span>299</span>
+                        </div>
+                        <div className="price-field">
+                            <span>Discount</span>
+                            <span>299</span>
+                        </div>
+                        <div className="price-field">
+                            <span>Delivery Charges</span>
+                            <span>299</span>
+                        </div>
+                        <div className="price-field highlight">
+                            <span>Total Amount</span>
+                            <span>₹300</span>
+                        </div>
+                    </div>
+                    <span className="text-center">You have saved ₹120, by shopping with us</span>
+
+                </div>
+                <button className="btn btn-primary">Place Order </button>
+            </div>
+        </section>
+    )
 }
--- src/pages/WishList/WishList.jsx
+++ src/pages/WishList/WishList.jsx
@@ -1,5 +1,61 @@
 export default function WishList() {
     return (
-        <div>This is wishlist</div>
+        <section class="container center-div wrap">
+
+            <div class="card card-vertical">
+                <img class="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
+                    <div class="card-content">
+                        <h3 class="card-title">Do Epic Shit</h3>
+                        <span class="text-center price highlight"> 299 </span>
+                        <button class="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
+                    </div>
+            </div>
+
+            <div class="card card-vertical">
+                <img class="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
+                    <div class="card-content">
+                        <h3 class="card-title">Do Epic Shit</h3>
+                        <span class="text-center price highlight"> 299 </span>
+                        <button class="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
+                    </div>
+            </div>
+
+            <div class="card card-vertical">
+                <img class="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
+                    <div class="card-content">
+                        <h3 class="card-title">Do Epic Shit</h3>
+                        <span class="text-center price highlight"> 299 </span>
+                        <button class="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
+                    </div>
+            </div>
+
+            <div class="card card-vertical">
+                <img class="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
+                    <div class="card-content">
+                        <h3 class="card-title">Do Epic Shit</h3>
+                        <span class="text-center price highlight"> 299 </span>
+                        <button class="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
+                    </div>
+            </div>
+
+            <div class="card card-vertical">
+                <img class="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
+                    <div class="card-content">
+                        <h3 class="card-title">Do Epic Shit</h3>
+                        <span class="text-center price highlight"> 299 </span>
+                        <button class="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
+                    </div>
+            </div>
+
+            <div class="card card-vertical">
+                <img class="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
+                    <div class="card-content">
+                        <h3 class="card-title">Do Epic Shit</h3>
+                        <span class="text-center price highlight"> 299 </span>
+                        <button class="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
+                    </div>
+            </div>
+
+        </section>
     )
 }
