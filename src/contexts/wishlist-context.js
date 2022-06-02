919701526aadcc66a4f7d1a4a63cfbce84865cca
Veeresh B V <Veereshbv04@gmail.com>
2022-06-03 01:16:02 +0530

    need to map cards to wishlistpage

--- src/contexts/wishlist-context.js
+++ src/contexts/wishlist-context.js
@@ -42,8 +42,37 @@ const WishlistProvider =({children})=>{
         }
     }
 
+    async function removeFromWishlist(product){
+        if (isLogged) {
+            try {
+                const response = await axios.delete(`/api/user/wishlist/:${product._id}`, {
+                    product
+                }, {
+                    headers: {
+                        authorization: encodedToken
+                    }
+                })
+                console.log("from wishlistcontext", response.status)
+                if (response.status === 201) {
+                    dispatch({
+                        type: "ADD_TO_WISHLIST",
+                        payload: response.data.wishlist
+                    })
+                    console.log("wishlist dispatch made", response.data.wishlist)
+                }
+            } catch (error) {
+                alert(error)
+            }
+
+
+
+        } else {
+            navigate("/login")
+        }
+    }
+
     return (
-        <wishlistContext.Provider value={{...initialState, addToWishlist}}>{children}</wishlistContext.Provider>
+        <wishlistContext.Provider value={{...initialState, addToWishlist, removeFromWishlist}}>{children}</wishlistContext.Provider>
     )
 }
 
