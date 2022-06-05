90d68962d4fab611c79521c002f8e3bb86f96070
Veeresh B V <Veereshbv04@gmail.com>
2022-06-05 16:42:17 +0530

    feat: wishlist done

--- src/pages/WishList/WishList.jsx
+++ src/pages/WishList/WishList.jsx
@@ -1,61 +1,15 @@
+import { Card } from "../../components"
+import { useWishlist } from "../../contexts/wishlist-context"
+
+
 export default function WishList() {
+    const { wishItems } = useWishlist()
+    console.log(wishItems)
     return (
         <section className="container center-div wrap">
-
-            <div className="card card-vertical">
-                <img className="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
-                    <div className="card-content">
-                        <h3 className="card-title">Do Epic Shit</h3>
-                        <span className="text-center price highlight"> 299 </span>
-                        <button className="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
-                    </div>
-            </div>
-
-            <div className="card card-vertical">
-                <img className="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
-                    <div className="card-content">
-                        <h3 className="card-title">Do Epic Shit</h3>
-                        <span className="text-center price highlight"> 299 </span>
-                        <button className="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
-                    </div>
-            </div>
-
-            <div className="card card-vertical">
-                <img className="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
-                    <div className="card-content">
-                        <h3 className="card-title">Do Epic Shit</h3>
-                        <span className="text-center price highlight"> 299 </span>
-                        <button className="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
-                    </div>
-            </div>
-
-            <div className="card card-vertical">
-                <img className="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
-                    <div className="card-content">
-                        <h3 className="card-title">Do Epic Shit</h3>
-                        <span className="text-center price highlight"> 299 </span>
-                        <button className="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
-                    </div>
-            </div>
-
-            <div className="card card-vertical">
-                <img className="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
-                    <div className="card-content">
-                        <h3 className="card-title">Do Epic Shit</h3>
-                        <span className="text-center price highlight"> 299 </span>
-                        <button className="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
-                    </div>
-            </div>
-
-            <div className="card card-vertical">
-                <img className="img-vertical" src="/assets/doepicshit.jpg" alt="sample image"/>
-                    <div className="card-content">
-                        <h3 className="card-title">Do Epic Shit</h3>
-                        <span className="text-center price highlight"> 299 </span>
-                        <button className="btn btn-primary"><a href="/pages/cart.html">Move to Cart</a></button>
-                    </div>
-            </div>
-
+            {wishItems.map(product =>(
+                <Card product={product} key={product._id}/>
+            ))}
         </section>
     )
 }
