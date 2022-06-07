6ed5bbb7bc65236f89f64680796d0b4960e2c256
Veeresh B V <Veereshbv04@gmail.com>
2022-06-08 01:31:45 +0530

    refactor

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,36 +5,28 @@ export default function Footer(){
                 <span className="footer-title">Get In touch
                 </span>
                 <ul>
-                    <li className="footer-links">abcdefghijklmn</li>
-                    <li className="footer-links">abcdefghijklmn</li>
-                    <li className="footer-links">abcdefghijklmn</li>
+                    <li className="footer-links">example@gmail.com</li>
+                    <li className="footer-links">+14 2323536838</li>
+                    <li className="footer-links">example.com</li>
                 </ul>
             </section>
             <section>
                 <span className="footer-title">Quick Links
                 </span>
                 <ul>
-                    <li className="footer-links">abcdefghijklmn</li>
-                    <li className="footer-links">abcdefghijklmn</li>
-                    <li className="footer-links">abcdefghijklmn</li>
+                    <li className="footer-links">About Us</li>
+                    <li className="footer-links">Privacy Terms</li>
+                    <li className="footer-links">Refund Policy</li>
                 </ul>
             </section>
-            <section>
-                <span className="footer-title">Information
-                </span>
-                <ul>
-                    <li className="footer-links">abcdefghijklmn</li>
-                    <li className="footer-links">abcdefghijklmn</li>
-                    <li className="footer-links">abcdefghijklmn</li>
-                </ul>
-            </section>
+            
             <section>
                 <span className="footer-title">Follow Us
                 </span>
                 <ul>
-                    <li className="footer-links">abcdefghijklmn</li>
-                    <li className="footer-links">abcdefghijklmn</li>
-                    <li className="footer-links">abcdefghijklmn</li>
+                    <li className="footer-links">Linkedin</li>
+                    <li className="footer-links">Facebook</li>
+                    <li className="footer-links">Instagram</li>
                     <span> 2022 Copyright Reserved</span>
                 </ul>
             </section>
