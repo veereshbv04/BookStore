d5d164211c700224460b5a78a1c987e5d58db600
Veeresh B V <Veereshbv04@gmail.com>
2022-06-08 20:02:46 +0530

    style

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -17,9 +17,9 @@ export default function Card({ product }) {
 
                 <div>
                     <span style={{ marginRight: "4rem" }}>{author}</span>
-                    <span className="price highlight">{discounted} </span>
+                    {/* <span className="price highlight">{discounted} </span> */}
                     <span className="price highlight"><strong>{original}  </strong></span>
-                    <span>{rating}</span>
+                    <span>⭐{rating}</span>
                 </div>
                 {
                     cart.find(item => item._id === _id) ? <button className="btn btn-primary"><Link to="/cart">Go to Cart</Link></button> : <button className="btn btn-primary"><a to="/cart" onClick={() => addToCart(product)}>Add to Cart</a></button>
